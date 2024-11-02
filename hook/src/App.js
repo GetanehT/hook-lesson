2a13f9a3a52ce2657e1558fdb59c753dd0be3ca7
Getaneh Biratu <mrgetaneh@gmail.com>
2024-11-02 02:12:38 +0000

    add forms

---
+++ hook/src/App.js
@@ -0,0 +1,14 @@
+import ControlledFormHooks from './components/controlledFormHooks';
+import './App.css';
+import UseStateWithArrays from './components/UseStateWithArrays';
+
+function App() {
+  return (
+    <div className="App">
+     <ControlledFormHooks />
+     <UseStateWithArrays />
+    </div>
+  );
+}
+
+export default App;
